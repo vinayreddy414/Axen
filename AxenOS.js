cb7f6b76c21c5dea26b0afb87f1b560d6dbf8ee5
vinayreddy414 <VINAYREDDYGEETLA123@gmail.com>
2026-04-18 10:47:51 +0530

    Create AxenOS.js

---
+++ AxenOS.js
@@ -0,0 +1,9 @@
+// AXEN EMPIRE: GENESIS CORE
+const Axen = {
+  status: "INITIALIZED",
+  boot: () => {
+    console.log("Axen Empire is live. The sovereign builder economy begins now.");
+  }
+};
+
+Axen.boot();
