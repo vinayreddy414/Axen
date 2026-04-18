50deeb75e323f0d28b78528806fb2b2694af1a8e
vinayreddy414 <VINAYREDDYGEETLA123@gmail.com>
2026-04-18 16:30:56 +0530

    Delete AxenOS.js

--- AxenOS.js
+++
@@ -1,9 +0,0 @@
-// AXEN EMPIRE: GENESIS CORE
-const Axen = {
-  status: "INITIALIZED",
-  boot: () => {
-    console.log("Axen Empire is live. The sovereign builder economy begins now.");
-  }
-};
-
-Axen.boot();
