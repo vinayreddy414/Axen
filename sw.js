a02ec84d0f4033cce3efaffbd1a0ed32f37e81d6
vinayreddy414 <VINAYREDDYGEETLA123@gmail.com>
2026-04-18 16:42:20 +0530

    Create sw.js (service worker) file

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Service Worker for AXEN Empire PWA
+
+const CACHE_NAME = 'axen-empire-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        }).catch(() => {
+            return caches.match('./index.html');
+        })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
